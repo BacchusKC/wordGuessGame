75a47511103140bbab47ca8a475462775ff46031
Travis Morris <Bacchuskc@gmail.com>
2018-12-06 05:01:54 -0600

    Beta 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,36 +5,20 @@ var losses = 0;
 var lives = 7;
 var word = "";
 var guess = "";
-var wrongGuess = [];
-var rightGuess = [];
 var lettersGuessed = [];
 var displayedWord = "";
-// var letter_buttons = [];
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var pick = "Pick a Letter";
 var lost = "Sorry, you lost...";
 var won = "You Won!";
 var gameover = true;
-var pressStart = "Press Start" 
+var pressStart = "Press Start"
+var hitReset = "Press Reset to play again." 
 
 scoreFunction ();
-// alert("Start button on controller, reset button on console");
+alert("Start button on controller, reset button on console");
 
-function startGame () {
-    gameover = false;
-
-}
-function scoreFunction () {
-    document.getElementById("userWins").innerHTML=wins;
-    document.getElementById("userLosses").innerHTML=losses;
-    document.getElementById("userLives").innerHTML=lives;
-};
-function getWord () {
-    word = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(word);
-    gameover = false;
-}
-var buttons = function () {
+function buttons() {
     myButtons = document.getElementById("letterButtons");
     letters = document.createElement("ul");
 
@@ -47,17 +31,10 @@ var buttons = function () {
       letters.appendChild(list);
     }
 }
-function letterSelect () {
-    var letter = event.target.innerHTML;
-    console.log(letter);
-    lettersGuessed.push(letter);
-    if (!contains(word, letter)) {
-        lives -= 1;
-        scoreFunction();
-    }
-    console.log(letter);
-    console.log(lettersGuessed)
-    run();
+function clear(){
+    document.getElementById("letterButtons").innerHTML = "";
+    document.getElementById("displayWord").innerHTML = "";
+    document.getElementById("replay").innerHTML = "";
 }
 function contains(arr, el) {
     for (var i = 0; i < arr.length; i++) {
@@ -65,14 +42,24 @@ function contains(arr, el) {
     }
     return false
 }
-function run () {
-    renderDisplayedWord();
-    testResult();
-    // if (gameover) {
-    //     DOM_game_message.innerHTML = game_complete;
-    // }
-    // renderButtons(game_complete);
+function getWord () {
+    word = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(word);
+    gameover = false;
 }
+function letterSelect () {
+    var letter = event.target.innerHTML;
+    if (!contains(lettersGuessed, letter)){
+        lettersGuessed.push(letter);
+        if (!contains(word, letter)) {
+            lives -= 1;
+            scoreFunction();
+        }
+        var l = event.target;
+        console.log(l);
+        event.target.classList.add("active");
+        run();
+}}
 function renderDisplayedWord () {
     displayedWord = "";
     for (var i = 0; i < word.length; i++) {
@@ -82,14 +69,27 @@ function renderDisplayedWord () {
         else {
             displayedWord += "_";
         }
-        // displayedWord += (i == word.length) ? "" : " " ;
     }
     document.getElementById("displayWord").innerHTML = displayedWord;
+}
+function run () {
+    renderDisplayedWord();
+    testResult();
+}
+function scoreFunction () {
+    document.getElementById("userWins").innerHTML=wins;
+    document.getElementById("userLosses").innerHTML=losses;
+    document.getElementById("userLives").innerHTML=lives;
+}
+function startListen(){
+    document.getElementById("alphabet").onclick = function() {
+    letterSelect()};
 }
 function testResult () {
     if (!contains(displayedWord, "_")) {
         wins++
         document.getElementById("letterButtons").innerHTML = "";
+        document.getElementById("replay").innerHTML = hitReset;
         win ();
         scoreFunction();
     }
@@ -97,6 +97,7 @@ function testResult () {
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/gameOver.jpg"/>';
         document.getElementById("topDisplay").innerHTML = lost;
         document.getElementById("letterButtons").innerHTML = "";
+        document.getElementById("replay").innerHTML = hitReset;
         losses++
         scoreFunction();
     }
@@ -105,44 +106,53 @@ function win () {
     if (displayedWord === "castlevania"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/castlevania.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "Castlevania";
     }else if (displayedWord === "contra"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/contra.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "Contra";
     }else if (displayedWord === "fortnite"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/fortnite.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "Fortnite";
     }else if (displayedWord === "mario"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/mario.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "Super Mario Bros";
     }else if (displayedWord === "metroid"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/metroid.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "Metroid";
     }else if (displayedWord === "minecraft"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/minecraft.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "Minecraft";
     }else if (displayedWord === "paperboy"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/paperboy.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "Paperboy";
     }else if (displayedWord === "pokemon"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/pokemon.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "Pokemon";
     }else if (displayedWord === "spiderman"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/spiderman.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "Spider-Man";
     }else if (displayedWord === "tetris"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/tetris.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "Tetris";
     }else if (displayedWord === "uncharted"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/uncharted.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "Uncharted";
     }else if(displayedWord === "zelda"){
         document.getElementById("tvImg").innerHTML = '<img id="screen" src="assets/images/zelda.jpg"/>';
         document.getElementById("topDisplay").innerHTML = won;
+        document.getElementById("displayWord").innerHTML = "The Legend of Zelda";
     }
 }
-
-
-
 
 document.getElementById("resetButton").onclick = function() {
     document.getElementById("topDisplay").innerHTML = pressStart;
@@ -164,11 +174,3 @@ document.getElementById("startButton").onclick = function() {
     renderDisplayedWord ()
     startListen();
 }}
-function startListen(){
-    document.getElementById("alphabet").onclick = function() {
-    letterSelect()};
-}
-function clear(){
-    document.getElementById("letterButtons").innerHTML = "";
-    document.getElementById("displayWord").innerHTML = "";
-}
